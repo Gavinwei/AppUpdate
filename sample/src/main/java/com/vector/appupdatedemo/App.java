a7d32f496b853c243a4c2268af35c3893401e759
gavinwead <13670877209@163.com>
2019-11-29 19:36:13 +0800

    初始化仓库

---
+++ sample/src/main/java/com/vector/appupdatedemo/App.java
@@ -0,0 +1,19 @@
+package com.vector.appupdatedemo;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.zhy.http.okhttp.OkHttpUtils;
+
+/**
+ * Created by Vector
+ * on 2017/7/17 0017.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkGo.getInstance().init(this);
+    }
+}
